6f1f832e69227ae2b1bf16b1f413691fb31fe882
shenlu89 <shenlu89dev@gmail.com>
2022-11-29 09:34:25 +0800

    style: add text underline offset in 3px

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      textUnderlineOffset: {
+        3: '3px',
+      },
+    },
   },
   plugins: [],
 }
