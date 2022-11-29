e2d3f9b5144d84e97d44f7db234f2334dd46f343
shenlu89 <shenlu89dev@gmail.com>
2022-11-30 00:07:54 +0800

    feat: add blog page

---
+++ pages/blog.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+
+const Blog: NextPage = () => {
+    return (
+        <>
+            <div className="flex flex-row relative justify-between w-full">
+                <div className="flex flex-col pr-8 relative w-full">
+                    <h1 className="font-extrabold text-3xl tracking-tight mb-1 text-black">Blog</h1>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Blog
