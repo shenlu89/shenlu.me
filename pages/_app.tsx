8e42fc7c2778c9e05544be34bc37ef4a26343a95
shenlu89 <shenlu89dev@gmail.com>
2022-11-29 09:43:22 +0800

    chore: add NextPage type

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import 'styles/globals.css'
+import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 import Layout from 'components/Layout'
 
-const App = ({ Component, pageProps }: AppProps) => {
+const App: NextPage<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
     <Layout>
       <Component {...pageProps} />
