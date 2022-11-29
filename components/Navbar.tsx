3c61d6beed33f4d750241a3e8823f60a56effcda
shenlu89 <shenlu89dev@gmail.com>
2022-11-29 09:37:59 +0800

    feat: add features on Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,40 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
+import { SunIcon } from '@heroicons/react/24/outline'
 
 const Navbar: NextPage = () => {
+    const navs = [
+        {
+            label: 'Home',
+            value: "/"
+        },
+        {
+            label: 'Blog',
+            value: '/blog'
+        },
+        {
+            label: 'About',
+            value: '/about'
+        }
+    ]
     return (
-        <div>
-            Navbar
-        </div>
+        <header className='flex justify-between max-w-3xl container mx-auto py-2'>
+            <nav className='flex flex-row'>
+                {
+                    navs?.map(nav => (
+                        <Link className='p-2 underline underline-offset-3 text-gray-600 hover:text-black' key={nav?.label} href={nav?.value}>
+                            {nav?.label}
+                        </Link>
+                    ))
+                }
+            </nav>
+            <div className='flex items-center mx-2'>
+                <div className='p-1 rounded-full border cursor-pointer bg-gray-600 hover:bg-black'>
+                    <SunIcon className="w-5 h-5 text-white" />
+                </div>
+            </div>
+        </header >
+
     )
 }
 
