0a98543685c09851fbd479f4aab79e46a4e08268
shenlu89 <shenlu89dev@gmail.com>
2022-12-01 01:15:38 +0800

    style: add dark mode heading for 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 const NotFound: NextPage = () => {
     return (
         <>
-            <h1 className="font-extrabold text-3xl tracking-tight mb-1 text-black">404</h1>
+            <h1 className="font-extrabold text-3xl tracking-tight mb-1 text-black dark:text-white">404</h1>
             <div className="text-gray-600 mb-4">
                 <span>Page Not Found. Go back to <Link href="/" className="underline hover:text-black underline-offset-3">Home</Link> page.</span>
             </div>
