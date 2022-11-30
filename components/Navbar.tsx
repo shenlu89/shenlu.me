5059eb73971c0c516286a329eae79c240cb4945a
shenlu89 <shenlu89dev@gmail.com>
2022-12-01 02:26:53 +0800

    refactor: extract headerNavLinks from Navbar and add it in data

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,32 +2,21 @@ import type { NextPage } from 'next'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
+import headerNavLinks from 'data/headerNavLinks'
 import ThemeSwitcher from './ThemeSwitcher'
+
 
 const Navbar: NextPage = () => {
     const { asPath } = useRouter()
-    const navs = [
-        {
-            text: 'Home',
-            href: "/"
-        },
-        {
-            text: 'Blog',
-            href: '/blog'
-        },
-        {
-            text: 'About',
-            href: '/about'
-        }
-    ]
+
 
     return (
         <header className='flex justify-between max-w-3xl container mx-auto py-2 text-gray-600 dark:text-gray-400'>
             <nav className='flex flex-row'>
                 {
-                    navs?.map(nav => (
-                        <Link className={`p-2 underline-offset-3 ${asPath === nav?.href ? 'font-bold text-black dark:text-white no-underline hover:underline' : 'underline hover:text-black dark:hover:text-white'}`} key={nav?.text} href={nav?.href}>
-                            {nav?.text}
+                    headerNavLinks?.map(nav => (
+                        <Link className={`p-2 underline-offset-3 ${asPath === nav?.href ? 'font-bold text-black dark:text-white no-underline hover:underline' : 'underline hover:text-black dark:hover:text-white'}`} key={nav?.title} href={nav?.href}>
+                            {nav?.title}
                         </Link>
                     ))
                 }
---
+++ data/headerNavLinks.ts
@@ -0,0 +1,16 @@
+const headerNavLinks = [
+    {
+        title: 'Home',
+        href: "/"
+    },
+    {
+        title: 'Blog',
+        href: '/blog'
+    },
+    {
+        title: 'About',
+        href: '/about'
+    }
+]
+
+export default headerNavLinks
