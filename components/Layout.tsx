590b6e2395f3043474d2bfc2672074bfabe73551
shenlu89 <shenlu89dev@gmail.com>
2022-12-07 15:49:37 +0800

    chore: update layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,26 +5,22 @@ import { PropsWithChildren } from 'react'
 
 import Navbar from 'components/Navbar'
 import Footer from 'components/Footer'
-import font from 'lib/font'
+import font from 'lib/fonts'
 import meta from 'data/metadata'
 
 const Layout: NextPage<PropsWithChildren> = ({ children }) => {
     return (
-        <>
+        <div className={`${font.className} text-black dark:text-white`}>
             <Head>
                 <title>{meta.title}</title>
                 <meta name="description" content={meta.description} />
-                <link rel="icon" href={meta.favicon} as="image" />
             </Head>
-            <div className={`${font.className} text-black dark:text-white`}>
-                <Navbar />
-                <main className="flex flex-col max-w-3xl container mx-auto px-2 py-6 h-full w-full">
-                    {children}
-                </main>
-                <Footer />
-            </div>
-        </>
-
+            <Navbar />
+            <main className="flex flex-col max-w-3xl container mx-auto px-2 py-6 h-full w-full">
+                {children}
+            </main>
+            <Footer />
+        </div>
     )
 }
 
