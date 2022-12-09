b0a62f526150530f57ccbdad05f2aafe77719e2e
shenlu89 <shenlu89dev@gmail.com>
2022-12-09 14:40:49 +0800

    feat: add prisma as database

---
+++ lib/db.ts
@@ -0,0 +1,21 @@
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+//
+// Learn more:
+// https://pris.ly/d/help/next-js-best-practices
+
+import { PrismaClient } from '@prisma/client'
+
+declare global {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined
+}
+
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  })
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
