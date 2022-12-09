923f628a6939e2d07dc3813e5901ee8d15307ee3
shenlu89 <shenlu89dev@gmail.com>
2022-12-09 23:12:53 +0800

    feat: add interactive functions in search input

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,6 +1,11 @@
+import { useState } from 'react'
 import type { NextPage } from 'next'
+import { MagnifyingGlassIcon, BackspaceIcon } from '@heroicons/react/24/outline'
 
 const Blog: NextPage = () => {
+    const [searchArticles, setSearchArticles] = useState('')
+    const handleClear = () => setSearchArticles('')
+
     return (
         <div className="flex flex-col relative w-full">
             <h1 className="font-extrabold text-3xl tracking-tight mb-1">Blog</h1>
@@ -8,12 +13,15 @@ const Blog: NextPage = () => {
                 {`I've written ** articles since 2019, using the search below to filter by title.`}
             </p>
             <div className="relative w-full mb-4">
+                <MagnifyingGlassIcon className="flex left-2 top-1/2 translate-y-[-50%] absolute w-5 h-5 text-gray-400" />
+                <BackspaceIcon onClick={handleClear} className={`${searchArticles ? '' : 'hidden'} flex right-2 top-1/2 translate-y-[-50%] absolute w-5 h-5 text-gray-600 cursor-pointer`} />
                 <input
                     aria-label="Search articles"
                     type="text"
-                    // onChange={(e) => setSearchValue(e.target.value)}
-                    placeholder="Search articles"
-                    className="block w-full px-4 py-2 text-black bg-white dark:bg-gray-200 border border-gray-400 dark:border-gray-200 rounded-sm focus:outline-0 focus:border-black dark:focus:bg-white"
+                    onChange={(e) => setSearchArticles(e.target.value)}
+                    placeholder="Search all posts"
+                    value={searchArticles}
+                    className="w-full px-8 py-2 text-black bg-white dark:border-black dark:focus:border-black border border-gray-400 rounded-sm focus:outline-0 focus:border-gray-600"
                 />
             </div>
         </div>
