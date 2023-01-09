11f6f0a020dbb4badb9bbc334d33f562253d3bf7
shenlu89 <shenlu89dev@gmail.com>
2023-01-09 19:14:01 +0800

    chore: update 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,15 +3,30 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 const NotFound: NextPage = () => {
-    return (
-        <>
-            <h1 className="font-extrabold text-3xl tracking-tight mb-1">404</h1>
-            <div className="text-gray-600 dark:text-gray-400 mb-4">
-                <span>Page Not Found. Go back to <Link href="/" className="underline hover:text-black dark:hover:text-white underline-offset-3">Home</Link> page.</span>
-            </div>
-            <Image width={400} height={400} src="/404.jpeg" alt={'Drawing hands by M.C. Escher'} priority />
-        </>
-    )
+  return (
+    <>
+      <h1 className="font-extrabold text-3xl tracking-tight mb-1">404</h1>
+      <div className="text-gray-600 dark:text-gray-400 mb-4">
+        <span>
+          Page Not Found. Go back to{' '}
+          <Link
+            href="/"
+            className="underline hover:text-black dark:hover:text-white underline-offset-3"
+          >
+            Home
+          </Link>{' '}
+          page.
+        </span>
+      </div>
+      <Image
+        width={400}
+        height={400}
+        src="/404.jpeg"
+        alt={'Drawing hands by M.C. Escher'}
+        priority
+      />
+    </>
+  )
 }
 
 export default NotFound
