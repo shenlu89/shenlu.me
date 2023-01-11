7f5573866f3ca272149d2940df36362f78f29731
shenlu89 <shenlu89dev@gmail.com>
2023-01-11 11:46:38 +0800

    refactor: rename mdxToHtml to mdxUtils

--- lib/mdxUtils.ts
+++ lib/mdxUtils.ts
@@ -1,3 +1,5 @@
+import fs from 'fs'
+import path from 'path'
 import { serialize } from 'next-mdx-remote/serialize'
 import readingTime from 'reading-time'
 import remarkGfm from 'remark-gfm'
@@ -5,6 +7,15 @@ import rehypeSlug from 'rehype-slug'
 import rehypeCodeTitles from 'rehype-code-titles'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypePrism from 'rehype-prism-plus'
+
+// POSTS_PATH is useful when you want to get the path to a specific file
+export const POSTS_PATH = path.join(process.cwd(), '_posts')
+
+// postFilePaths is the list of all mdx files inside the POSTS_PATH directory
+export const postFilePaths = fs
+  .readdirSync(POSTS_PATH)
+  // Only include md(x) files
+  .filter((path) => /\.mdx?$/.test(path))
 
 export async function mdxToHtml(source: string) {
   const mdxSource = await serialize(source, {
