387d07e51b378669142f2230d126f411eacc874a
shenlu89 <shenlu89dev@gmail.com>
2023-01-13 02:33:01 +0800

    style: setup tailwind typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,19 @@
 module.exports = {
   darkMode: 'class',
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
       colors: {
         twitter: '#1DA1F2'
-      },
-    },
+      }
+    }
   },
-  plugins: [],
+  variants: {
+    typography: ['dark']
+  },
+  plugins: [require('@tailwindcss/typography')]
 }
