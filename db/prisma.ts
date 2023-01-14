83191f53ce12a005b60e049a45aa6401a94075d0
shenlu89 <shenlu89dev@gmail.com>
2023-01-14 14:27:46 +0800

    feat: add prisma generate command

--- db/prisma.ts
+++ db/prisma.ts
@@ -15,7 +15,7 @@ declare global {
 export const prisma =
   global.prisma ||
   new PrismaClient({
-    log: ['query'],
+    log: ['query']
   })
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma
