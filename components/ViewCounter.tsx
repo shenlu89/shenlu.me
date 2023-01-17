e01d0c390375ebbc9dffb1d2610ac02493318aa9
shenlu89 <shenlu89dev@gmail.com>
2023-01-17 23:06:27 +0800

    feat: add view counter component

---
+++ components/ViewCounter.tsx
@@ -0,0 +1,33 @@
+import { useEffect } from 'react'
+import useSWR from 'swr'
+import type { NextPage } from 'next'
+import { EyeSlashIcon } from '@heroicons/react/24/outline'
+
+import fetcher from 'lib/fetcher'
+import type { Views, Slug } from 'lib/types'
+
+const ViewCounter: NextPage<Slug> = ({ slug, method }) => {
+  const { data } = useSWR<Views>(`/api/views/${slug}`, fetcher)
+  const views = new Number(data?.total)
+  const registerView = async () =>
+    await fetch(`/api/views/${slug}`, {
+      method
+    })
+
+  useEffect(() => {
+    registerView()
+  }, [slug])
+
+  return (
+    <span className="flex items-center justify-end">
+      {views > 0 ? (
+        views.toLocaleString()
+      ) : (
+        <EyeSlashIcon className="w-4 h-4 mr-1" />
+      )}
+      {' views'}
+    </span>
+  )
+}
+
+export default ViewCounter
