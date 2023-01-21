f69c7e9057cb0b90c1f6870234412e855e0090a9
shenlu89 <shenlu89dev@gmail.com>
2023-01-21 17:43:44 +0800

    feat: add custom svg icons

---
+++ data/icons/index.ts
@@ -0,0 +1,7 @@
+export { CheckIcon } from './check'
+export { CopyIcon } from './copy'
+export { DiscordIcon } from './discord'
+export { TwitterIcon } from './twitter'
+export { GitHubIcon } from './github'
+export { MoonIcon } from './moon'
+export { SunIcon } from './sun'
