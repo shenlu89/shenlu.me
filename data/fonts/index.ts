46a17ad69cf157c092b934971899858e8e91f77a
shenlu89 <shenlu89dev@gmail.com>
2023-01-24 11:20:31 +0800

    chore: change fonts position

---
+++ data/fonts/index.ts
@@ -0,0 +1,22 @@
+import localFont from '@next/font/local'
+const font = localFont({
+  src: [
+    {
+      path: '../../public/fonts/cantarell-regular.woff2',
+      weight: '400',
+      style: 'normal'
+    },
+    {
+      path: '../../public/fonts/cantarell-bold.woff2',
+      weight: '700',
+      style: 'bold'
+    },
+    {
+      path: '../../public/fonts/cantarell-extrabold.woff2',
+      weight: '800',
+      style: 'extrabold'
+    }
+  ]
+})
+
+export default font
