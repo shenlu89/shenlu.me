4db8113788c9deed5d8eceb4d5edb828614ba65c
shenlu89 <shenlu89dev@gmail.com>
2023-01-26 00:48:20 +0800

    style: custom prose class styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,44 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}'
   ],
+  theme: {
+    extend: {
+      colors: {
+        'white': '#fff',
+        'black': '#000',
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            a: {
+              color: theme('colors.blue.600'),
+              '&:hover': {
+                color: theme('colors.blue.800')
+              },
+              code: { color: theme('colors.blue.600') }
+            },
+            code: { color: theme('colors.black') },
+            h1: {
+              fontSize: theme('fontSize.3xl'),
+              letterSpacing: theme('letterSpacing.tight')
+            },
+          }
+        },
+        invert: {
+          css: {
+            a: {
+              color: theme('colors.blue.400'),
+              '&:hover': {
+                color: theme('colors.blue.600')
+              },
+              code: { color: theme('colors.blue.400') }
+            },
+            code: { color: theme('colors.white') },
+          }
+        }
+      })
+    }
+  },
   variants: {
     typography: ['dark']
   },
