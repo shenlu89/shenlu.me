e43817b61214c1229d04286241ee6a4e755323a7
shenlu89 <shenlu89dev@gmail.com>
2023-02-04 02:00:49 +0800

    change meta from ts to json

--- data/metadata.ts
+++
@@ -1,7 +0,0 @@
-const metadata = {
-    title: 'Shen Lu - Developer, Writer and Creator.',
-    description: `Shen Lu's Personal Website`,
-    favicon: '/images/logo.svg'
-}
-
-export default metadata
