84652ee09e65a143ce6f2b36ee7bb7015fe1c114
shenlu89 <shenlu89dev@gmail.com>
2023-02-05 09:48:43 +0800

    chore: update home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,29 @@
 import type { NextPage } from 'next'
 import Image from 'next/image'
 
-const Home: NextPage = () => {
+const Home: NextPage = (props) => {
   return (
-    <>
-      <div className="flex flex-row relative justify-between w-full">
-        <div className="flex flex-col pr-8 relative w-full">
-          <h1 className="font-extrabold text-3xl tracking-tight mb-1">Shen Lu</h1>
-          <h2 className="text-gray-600 dark:text-gray-400 mb-4">Developer, Writer and Creator.</h2>
-          <p className="text-gray-600 dark:text-gray-400 mb-16">Now I'm focusing on web development and data visualization.</p>
-        </div>
-        <div className="relative mr-auto">
-          <Image className="rounded-full" alt="Shen Lu"
-            width={120} height={120} src={'/images/avatar.jpg'} priority />
-        </div>
+    <div className="flex flex-row relative justify-between w-full">
+      <div className="flex flex-col pr-8 relative w-full">
+        <h1 className="font-extrabold text-3xl tracking-tight mb-0">Shen Lu</h1>
+        <span className="text-gray-600 dark:text-gray-400">
+          Developer, Writer and Creator.
+        </span>
+        <p className="text-gray-600 dark:text-gray-400">
+          Now I'm focusing on web development and data visualization.
+        </p>
       </div>
-    </>
+      <div className="relative mr-auto">
+        <Image
+          className="rounded-full"
+          alt="Shen Lu"
+          width={120}
+          height={120}
+          src={'/images/avatar.jpg'}
+          priority
+        />
+      </div>
+    </div>
   )
 }
 
