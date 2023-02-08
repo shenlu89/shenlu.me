23724abafbe954acf810ddd51057d725ce6509c1
shenlu89 <shenlu89dev@gmail.com>
2023-02-09 01:31:16 +0800

    chore: move fonts from data to lib folder

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,22 +1,23 @@
 import localFont from '@next/font/local'
-const font = localFont({
+
+const fonts = localFont({
   src: [
     {
-      path: '../../public/fonts/cantarell-regular.woff2',
+      path: '../public/fonts/cantarell-regular.woff2',
       weight: '400',
       style: 'normal'
     },
     {
-      path: '../../public/fonts/cantarell-bold.woff2',
+      path: '../public/fonts/cantarell-bold.woff2',
       weight: '700',
       style: 'bold'
     },
     {
-      path: '../../public/fonts/cantarell-extrabold.woff2',
+      path: '../public/fonts/cantarell-extrabold.woff2',
       weight: '800',
       style: 'extrabold'
     }
   ]
 })
 
-export default font
+export default fonts
