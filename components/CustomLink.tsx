58a5da23ceaf3e185e6ecfd4a8f1c5ec71f69462
shenlu89 <shenlu89dev@gmail.com>
2023-02-16 11:41:54 +0800

    fix: custom link bug

--- components/CustomLink.tsx
+++ components/CustomLink.tsx
@@ -2,7 +2,10 @@ import Link from 'next/link'
 
 const CustomLink = (props: any) => {
   const { href } = props
-  if (!href.startsWith('/') && !href.startsWith('#')) {
+  if (href.startsWith('#')) {
+    return <a {...props} />
+  }
+  if (!href.startsWith('/')) {
     props = {
       target: '_blank',
       rel: 'noopener noreferrer',
