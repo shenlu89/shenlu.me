a0545f2b732af7d81bd8fb641f8270d7c2e4a6b0
shenlu89 <shenlu89dev@gmail.com>
2023-02-20 09:20:10 +0800

    feat: add google adsense

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-import meta from 'data/metadata'
-
-export default function Document () {
-    return (
-        <Html lang="en">
-            <Head>
-                <link rel="icon" href={meta.favicon} as="image" />
-            </Head>
-            <body className="flex flex-col h-screen">
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="icon" href={'/images/logo.svg'} as="image" />
+        <script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_CLIENT}`}
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <body className="flex flex-col h-screen">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
