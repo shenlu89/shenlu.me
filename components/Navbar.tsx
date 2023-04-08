8159aac03e5c1c203548441a1e4dc99b3682b426
shenlu89 <shenlu89dev@gmail.com>
2023-04-08 15:56:23 +0800

    chore: add li tag and key

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,23 +9,26 @@ const Navbar: NextPage = () => {
   const { asPath } = useRouter()
 
   return (
-    <header className="flex justify-between max-w-3xl container mx-auto py-2">
-      <nav className="flex flex-row">
-        {headerNavLinks?.map((nav) => (
-          <Link
-            className={`p-2 underline-offset-4 hover:underline outline-none ${
-              asPath === nav?.href
-                ? 'font-bold no-underline'
-                : 'text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-gray-200'
-            }`}
-            key={nav?.title}
-            href={nav?.href}
-          >
-            {nav?.title}
-          </Link>
-        ))}
+    <header className="flex max-w-3xl container mx-auto py-2">
+      <nav className="flex flex-1">
+        <ul className="flex">
+          {headerNavLinks?.map((nav) => (
+            <li key={nav?.title}>
+              <Link
+                className={`flex p-2 underline-offset-4 hover:underline outline-none ${
+                  asPath === nav?.href
+                    ? 'font-bold no-underline'
+                    : 'text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-gray-200'
+                }`}
+                href={nav?.href}
+              >
+                {nav?.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </nav>
-      <div className="flex items-center mx-2">
+      <div className="flex items-center mr-2">
         <ThemeSwitcher />
       </div>
     </header>
