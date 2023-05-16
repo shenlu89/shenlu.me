3b01ce05e69d72a19a42e75fa833f4a12da127c0
shenlu89 <shenlu89dev@gmail.com>
2023-05-16 10:18:26 +0800

    feat: add dynamic route page for blog

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,90 @@
+import 'app/highlighting.css'
+import { Metadata } from 'next'
+import { allPosts, Post } from 'contentlayer/generated'
+import { notFound } from 'next/navigation'
+
+import Image from 'next/image'
+import ViewCounter from 'components/ViewCounter'
+import Comment from 'components/Comment'
+import { useMDXComponent } from 'next-contentlayer/hooks'
+
+import CustomLink from 'components/CustomLink'
+import CustomPre from 'components/CustomPre'
+
+export const runtime = 'edge'
+
+const MDXComponent = {
+  a: CustomLink,
+  pre: CustomPre
+}
+
+export async function generateMetadata({
+  params
+}: {
+  params: { slug: string }
+}): Promise<Metadata | undefined> {
+  const post = allPosts.find((post) => post.slug === params.slug) as Post
+  if (!post) return
+  const { title, slug, publishedAt: publishedTime } = post
+  // const ogImage = image
+  return {
+    title,
+    openGraph: {
+      title,
+      type: 'article',
+      publishedTime,
+      url: `https://shenlu.me/blog/${slug}`,
+      images: []
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      images: []
+    }
+  }
+}
+
+export default function PostPage({ params }: { params: { slug: string } }) {
+  const post = allPosts.find((post) => post.slug === params.slug) as Post
+  if (!post) notFound()
+  const MDXContent = useMDXComponent(post.body.code)
+  return (
+    <article className="flex flex-col items-start justify-center w-full">
+      <h1 className="font-extrabold text-2xl tracking-tight mb-0">
+        {post.title}
+      </h1>
+      <div className="flex items-start justify-between w-full md:flex-row my-8">
+        <div className="flex items-center">
+          <Image
+            alt="Shen Lu"
+            height={36}
+            width={36}
+            sizes="20vw"
+            src="/images/avatar.jpg"
+            className="rounded-full mt-0 mb-0"
+            priority
+          />
+          <div className="ml-2 text-sm text-gray-600 dark:text-gray-400">
+            <span className="flex text-black dark:text-gray-200 font-bold">
+              Shen Lu
+            </span>
+            <div>Posted on {post.publishedAt}</div>
+          </div>
+        </div>
+        <div className="flex flex-col text-sm text-gray-600 dark:text-gray-400 min-w-32 md:mt-0">
+          <ViewCounter slug={post.slug} method={'POST'} />
+          <div>
+            {post.readingTime}
+            {` (`}
+            {post.wordCount}
+            {' words)'}
+          </div>
+        </div>
+      </div>
+      <div className="w-full prose dark:prose-invert max-w-none mb-8">
+        <MDXContent components={MDXComponent} />
+      </div>
+      <Comment />
+    </article>
+  )
+}
