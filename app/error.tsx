158c6e16c265f9923d6203c09003024939f720be
shenlu89 <shenlu89dev@gmail.com>
2023-08-03 13:58:48 +0800

    feat: add error page

---
+++ app/error.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { useEffect } from 'react'
+
+export default function Error({
+  error,
+  reset
+}: {
+  error: Error
+  reset: () => void
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error)
+  }, [error])
+
+  return (
+    <div>
+      <p>Oh no, something went wrong... maybe refresh?</p>
+      <button onClick={() => reset()}>Try again</button>
+    </div>
+  )
+}
