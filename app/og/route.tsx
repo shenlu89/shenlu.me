15564eea95ca3404769c526ba65c14db7c87d3ad
shenlu89 <shenlu89dev@gmail.com>
2023-08-06 02:00:15 +0800

    chore: remove the default width and height

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -80,8 +80,6 @@ export async function GET(request: Request) {
         </div>
       ),
       {
-        width: 1200,
-        height: 630,
         fonts: [
           {
             name: 'Cantarell',
@@ -99,7 +97,6 @@ export async function GET(request: Request) {
       }
     )
   } catch (e: any) {
-    console.log(`${e.message}`)
     return new Response(`Failed to generate the image`, {
       status: 500
     })
