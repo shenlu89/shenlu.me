5a75fd0aea40461351e8f09d77fdb8b4a567dbba
shenlu89 <shenlu89dev@gmail.com>
2023-08-07 10:21:57 +0800

    style: modify border radius

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -32,7 +32,7 @@ const Blog = () => {
           {`, using the search below to filter by title.`}
         </p>
         <div className="relative w-full mb-4">
-          <MagnifyingGlassIcon className="flex left-3 top-1/2 translate-y-[-50%] absolute w-5 h-5 text-gray-600" />
+          <MagnifyingGlassIcon className="flex left-3 top-1/2 translate-y-[-50%] absolute w-5 h-5 text-gray-400" />
           <XCircleIcon
             onClick={() => {
               searchInput.current?.focus()
@@ -40,7 +40,7 @@ const Blog = () => {
             }}
             className={`${
               !serachPosts && 'hidden'
-            } flex right-2 top-1/2 translate-y-[-50%] absolute w-5 h-5 text-gray-600 hover:text-black cursor-pointer`}
+            } flex right-2 top-1/2 translate-y-[-50%] absolute w-5 h-5 text-gray-400 hover:text-black cursor-pointer`}
           />
           <input
             autoFocus
@@ -50,12 +50,12 @@ const Blog = () => {
             onChange={(e) => setserachPosts(e.target.value)}
             placeholder="Search all posts"
             value={serachPosts}
-            className="w-full px-10 py-2 text-black bg-white dark:border-black dark:focus:border-black border focus:border-gray-400 rounded-sm focus:outline-0"
+            className="w-full px-10 py-2 text-black bg-white focus:bg-gray-50 dark:border-black dark:focus:border-black border rounded focus:outline-0"
           />
         </div>
       </div>
       {!filteredBlogPosts.length && (
-        <div className="flex py-4 text-black dark:text-gray-400 items-center justify-center">
+        <div className="flex py-4 text-gray-400 mt-32 dark:text-gray-400 items-center justify-center">
           <EyeSlashIcon className="w-5 h-5 mr-2" />
           <span>No Posts Found.</span>
         </div>
@@ -65,7 +65,7 @@ const Blog = () => {
           <Link key={post.slug} href={post.url}>
             <li
               key={post.slug}
-              className="flex flex-col w-full dark:hover:bg-gray-900 border hover:bg-gray-50 rounded-sm p-4 mb-4 dark:border-gray-600 dark:bg-black hover:shadow-sm dark:hover:shadow-sm"
+              className="flex flex-col w-full dark:hover:bg-gray-900 border hover:bg-gray-50 rounded p-4 mb-4 dark:border-gray-600 dark:bg-black hover:shadow-sm dark:hover:shadow-sm"
             >
               <span className="font-bold">{post.title}</span>
               <div className="flex justify-between">
