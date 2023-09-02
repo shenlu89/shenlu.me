f0e0e0d8dd4baf008c1eef5ebadfb4b4f533c314
shenlu89 <shenlu89dev@gmail.com>
2023-09-02 11:05:07 +0800

    chore: change the icon and layout styling

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -55,9 +55,9 @@ const Blog = () => {
         </div>
       </div>
       {!filteredBlogPosts.length && (
-        <div className="flex py-4 text-gray-400 mt-32 dark:text-gray-400 items-center justify-center">
-          <EyeSlashIcon className="w-5 h-5 mr-2" />
-          <span>No Posts Found.</span>
+        <div className="flex flex-col py-8 text-gray-600 items-center dark:text-gray-400 justify-center">
+          <MagnifyingGlassIcon className="w-10 h-10 p-2 bg-gray-100 dark:text-gray-600 rounded-full mb-2" />
+          <span>No posts found for '{serachPosts}'</span>
         </div>
       )}
       <ul>
