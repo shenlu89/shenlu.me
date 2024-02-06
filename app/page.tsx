60b9a89616df151e17c46ac519bf75391c1bf3a9
shenlu89 <shenlu89dev@gmail.com>
2024-02-07 00:50:20 +0800

    chore: add meta data and update page information

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,30 @@
-import Image from 'next/image';
+import Image from "next/image";
+import { HomePage } from "@/app/data/meta-data";
 
 export default function Home() {
   return (
-    <div className='flex flex-row relative justify-between w-full'>
-      <div className='flex flex-col relative w-full'>
-        <h1 className='font-extrabold text-2xl tracking-tight mb-4'>Shen Lu</h1>
-        <p className='text-gray-600 dark:text-gray-400 mb-4'>
-          Developer and Writer
-        </p>
-        <p className='text-gray-600 dark:text-gray-400'>
-          All good things come to those who wait.
-        </p>
-      </div>
-      <div className='relative mr-auto'>
+    <div className="flex flex-col space-y-8">
+      <div className="flex flex-col-reverse space-y-4 md:space-y-0 space-y-reverse text-center md:text-left md:flex-row relative item-center justify-between w-full">
+        <div className="flex flex-col relative w-full space-y-4 justify-center">
+          <h1 className="font-extrabold text-2xl tracking-tight">
+            {HomePage.title}
+          </h1>
+          <h2 className="font-bold capitalize">{HomePage.description}</h2>
+          <p className="mb-4 text-slate-600 dark:text-slate-400">
+            {HomePage.motto}
+          </p>
+        </div>
         <Image
-          className='rounded-full'
-          alt='Shen Lu'
-          width={120}
-          height={120}
-          src={'/images/avatar.jpg'}
+          className="flex relative mx-auto md:mr-auto rounded-full"
+          alt={HomePage.title}
+          width={110}
+          height={110}
+          src={HomePage.avatar_url}
           priority
         />
       </div>
+      <hr className="border-slate-200 dark:border-slate-600" />
+      <div className="flex flex-col relative w-full space-y-4"></div>
     </div>
   );
 }
