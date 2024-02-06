2613d6c4911ba80de9899a0b2744919c22e7f8c2
shenlu89 <shenlu89dev@gmail.com>
2024-02-07 01:01:43 +0800

    refactor: move db to app folder

--- app/db/prisma.ts
+++ app/db/prisma.ts
@@ -4,18 +4,18 @@
 // Learn more:
 // https://pris.ly/d/help/next-js-best-practices
 
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
 declare global {
   // allow global `var` declarations
   // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined
+  var prisma: PrismaClient | undefined;
 }
 
 export const prisma =
   global.prisma ||
   new PrismaClient({
-    log: ['query']
-  })
+    log: ["query"],
+  });
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
