74efec5fd57f7ab75c0221af39b3b888a50aac6e
shenlu89 <shenlu89dev@gmail.com>
2024-02-07 00:47:51 +0800

    chore: update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,82 +1,31 @@
-import './globals.css'
-import 'katex/dist/katex.min.css'
-import type { Metadata } from 'next'
-import localFont from 'next/font/local'
-import Providers from '@/components/Providers'
+import "./globals.css";
+import "katex/dist/katex.min.css";
 
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
-import Script from 'next/script'
+import type { Metadata } from "next";
+import Providers from "@/app/components/providers";
+import Header from "@/app/components/header";
+import Footer from "@/app/components/footer";
+import Script from "next/script";
 
-const fonts = localFont({
-  src: [
-    {
-      path: '../public/fonts/cantarell-regular.woff2',
-      weight: '400',
-      style: 'normal'
-    },
-    {
-      path: '../public/fonts/cantarell-bold.woff2',
-      weight: '700',
-      style: 'bold'
-    },
-    {
-      path: '../public/fonts/cantarell-extrabold.woff2',
-      weight: '800',
-      style: 'extrabold'
-    }
-  ]
-})
+import LocalFont from "@/app/lib/local-font";
+import { HomePage } from "@/app/data/meta-data";
 
-export const metadata: Metadata = {
-  metadataBase: new URL('https://shenlu.me'),
-  title: {
-    default: 'Shen Lu',
-    template: '%s | Shen Lu'
-  },
-  description: 'Developer and Writer.',
-  openGraph: {
-    title: 'Shen Lu',
-    description: 'Developer and Writer.',
-    url: 'https://shenlu.me',
-    siteName: 'Shen Lu',
-    locale: 'en-US',
-    type: 'website'
-  },
-  twitter: {
-    title: 'Shen Lu',
-    card: 'summary_large_image'
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1
-    }
-  },
-  icons: {
-    shortcut: '/favicon.ico'
-  }
-}
+export const metadata: Metadata = HomePage.metadata;
 
 export default function RootLayout({
-  children
+  children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body
-        suppressHydrationWarning={true}
-        className={`flex flex-col h-screen ${fonts.className}`}
+        className={`flex flex-col h-screen ${LocalFont.className}`}
+        suppressHydrationWarning
       >
         <Providers>
-          <Navbar />
-          <main className="max-w-3xl container text-black dark:text-white mx-auto px-4 py-6 flex-1 leading-6">
+          <Header />
+          <main className="max-w-3xl container text-black dark:text-white mx-auto px-6 py-6 flex-1 leading-6">
             {children}
           </main>
           <Footer />
@@ -95,5 +44,5 @@ export default function RootLayout({
         </Script>
       </body>
     </html>
-  )
+  );
 }
