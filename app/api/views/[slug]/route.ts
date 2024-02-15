6671b2ed1263f575a8d02c3452fa23279f726f99
shenlu89 <shenlu89dev@gmail.com>
2024-02-15 18:39:18 +0800

    feat: add @upstash/redis

--- app/api/views/[slug]/route.ts
+++ app/api/views/[slug]/route.ts
@@ -1,23 +1,22 @@
 import { NextResponse } from "next/server";
-import { prisma } from "@/app/db/prisma";
+import { Redis } from "@upstash/redis";
+
+const redis = Redis.fromEnv();
 
 export async function GET(
-  request: Request,
-  { params }: { params: { slug: string } },
+  _request: Request,
+  { params }: { params: { slug: string } }
 ) {
   const { slug } = params;
 
   try {
-    const views = await prisma.views.findUnique({
-      where: {
-        slug,
-      },
-    });
+    const views = await redis.get(slug);
+
     return NextResponse.json(
       {
-        total: views?.count.toString(),
+        total: views?.toString(),
       },
-      { status: 200 },
+      { status: 200 }
     );
   } catch (e: any) {
     return NextResponse.json({ message: e.message }, { status: 500 });
@@ -25,27 +24,18 @@ export async function GET(
 }
 
 export async function POST(
-  request: Request,
-  { params }: { params: { slug: string } },
+  _request: Request,
+  { params }: { params: { slug: string } }
 ) {
   const { slug } = params;
   try {
-    const newOrUpdatedViews = await prisma.views.upsert({
-      where: { slug },
-      create: {
-        slug,
-      },
-      update: {
-        count: {
-          increment: 1,
-        },
-      },
-    });
+    const views = await redis.incr(slug);
+
     return NextResponse.json(
       {
-        total: newOrUpdatedViews.count.toString(),
+        total: views?.toString(),
       },
-      { status: 200 },
+      { status: 200 }
     );
   } catch (e: any) {
     return NextResponse.json({ message: e.message }, { status: 500 });
