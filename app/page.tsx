cbd0756034a67154cffd2296bb051cd9235e4c4e
shenlu89 <shenlu89dev@gmail.com>
2024-11-05 23:33:12 +0800

    chore: update home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,18 @@ export default function Home() {
     <div className="flex flex-col space-y-8">
       <div className="flex flex-col-reverse space-y-4 md:space-y-0 space-y-reverse text-center md:text-left md:flex-row relative item-center justify-between w-full">
         <div className="flex flex-col relative w-full space-y-4 justify-center">
-          <h1 className="font-extrabold text-2xl tracking-tight">
+          <h1 className="font-extrabold text-4xl tracking-tight">
             {HomePage.title}
           </h1>
-          <h2 className="font-bold capitalize">{HomePage.description}</h2>
-          <p className="mb-4 text-slate-600 dark:text-slate-400">
-            {HomePage.motto}
+          <p className="mb-4 text-lg text-slate-600 dark:text-slate-400">
+            {HomePage.description}
           </p>
         </div>
         <Image
           className="flex relative mx-auto md:mr-auto rounded-full"
           alt={HomePage.title}
-          width={110}
-          height={110}
+          width={90}
+          height={90}
           src={HomePage.avatar_url}
           priority
         />
