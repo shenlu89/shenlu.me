1dc663708e7095c157d867d1890c47ac73786995
shenlu89 <shenlu89dev@gmail.com>
2024-11-06 23:54:25 +0800

    chore: update work page

--- components/custom-image.tsx
+++ components/custom-image.tsx
@@ -10,7 +10,7 @@ export default function CustomImg({ src, alt, width, height, ...props }: any) {
       height={height || 0}
       sizes="100vw"
       style={{
-        width: "240px",
+        width: "360px",
         height: "auto",
       }}
       priority={true}
--- components/showcase.tsx
+++ components/showcase.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import CustomImage from "@/components/custom-image";
+// import CustomImage from "@/components/custom-image";
 import Image from "next/image";
 import { RiShareCircleLine } from "react-icons/ri";
 
@@ -9,11 +9,11 @@ const Showcase = ({ href, title, logoUrl, imageUrl, description }: any) => {
     <Link
       href={href}
       target="_blank"
-      className="flex flex-col justify-between md:w-1/2 w-full no-underline p-6 border bg-slate-50 hover:bg-slate-100 dark:hover:bg-slate-800 dark:bg-black dark:border-slate-600 border-slate-200 rounded"
+      className="flex flex-start justify-between w-full space-x-4 no-underline p-6 border bg-slate-50 hover:bg-slate-100 dark:hover:bg-slate-800 dark:bg-black dark:border-slate-600 border-slate-200 rounded"
     >
-      <div className="flex item-center md:flex-row justify-between space-y-4">
-        <div className="flex flex-col items-center md:items-start">
-          <div className="flex w-fulln flex-col sm:flex-row space-y-4 sm:space-y-0 justify-center md:justify-start md:flex-start space-x-0 sm:space-x-4">
+      <div className="flex item-center w-full md:flex-row justify-between space-y-4">
+        <div className="flex flex-col w-full items-center md:items-start">
+          <div className="flex w-full items-center flex-col sm:flex-row space-y-4 sm:space-y-0 justify-center md:justify-start md:flex-start space-x-0 sm:space-x-4">
             <Image
               src={logoUrl}
               alt={title}
@@ -21,7 +21,7 @@ const Showcase = ({ href, title, logoUrl, imageUrl, description }: any) => {
               height="48"
               className="flex self-center"
             />
-            <div className="flex flex-col justify-center items-center sm:items-start">
+            <div className="flex w-full flex-col justify-center items-center sm:items-start">
               <h3 className="flex text-lg font-extrabold m-0">{title}</h3>
               <p className="flex space-x-1 items-center m-0 p-0 text-slate-600 dark:text-slate-400">
                 <span>{href}</span>
@@ -30,16 +30,20 @@ const Showcase = ({ href, title, logoUrl, imageUrl, description }: any) => {
             </div>
           </div>
           <hr className="border border-x-transparent border-t-transparent my-4 border-b-slate-200 dark:border-b-slate-400 w-full px-4" />
-          <p className="flex text-start text-slate-600 dark:text-slate-200">
+          <p className="flex w-full text-start text-slate-600 dark:text-slate-200">
             {description}
           </p>
         </div>
       </div>
-      <CustomImage
-        src={imageUrl}
-        alt={title}
-        className="flex shadow self-center justify-center !ml-0 !mt-0"
-      />
+      <div>
+        <Image
+          src={imageUrl}
+          alt={title}
+          width={400}
+          height={0}
+          className="flex shadow self-center justify-center !ml-0 !mt-0"
+        />
+      </div>
     </Link>
   );
 };
