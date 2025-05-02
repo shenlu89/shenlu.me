ccc401fddaae71f7e1d315e0c8b4015b5f03bc22
shenlu89 <shenlu89dev@gmail.com>
2025-05-03 00:36:14 +0800

    fix: complie error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ['next/core-web-vitals'],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "react/no-unescaped-entities"
+    },
+  }),
+]
 
 export default eslintConfig;
